f18f9b5dc0398b6578e2b60836b1c475126c5361
Adarsh Bajpai <37613011+AdarshBajpai@users.noreply.github.com>
2019-12-21 20:01:26 +0530

    Directives
    Practicing directives

--- src/server/server.component.ts
+++ src/server/server.component.ts
@@ -2,13 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent {
   alloweNewServer = false;
   serverStatus = "No server created..";
   serverName = "";
   serverCreated = false;
+  serverOnlineStatus = Math.random() < 0.5 ? "offline" : "online";
 
   Constructor() {
     setTimeout(myFunction, 2000);
@@ -25,5 +33,9 @@ export class ServerComponent {
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  getColour() {
+    return this.serverOnlineStatus === "online" ? "green" : "red";
   }
 }
